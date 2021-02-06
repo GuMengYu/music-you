ae49d8bc770ab73b50530aefb2a49808453b3fd4
yuhong <1415515984yuri@gmail.com>
2021-02-06 13:09:39 +0800

    fix: 高度修正

--- src/views/index.vue
+++ src/views/index.vue
@@ -80,7 +80,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-$playerbarHeight: 70px;
+$playerbarHeight: 66px;
 .music-app {
   width: 100%;
   height: 100%;
@@ -88,7 +88,6 @@ $playerbarHeight: 70px;
   position: relative;
   transform: translate(0%);
   .playing-list {
-    top: 62px!important;
     height: calc(100% - #{$playerbarHeight}) !important;
   }
   .right {
