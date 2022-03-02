51f935eef6c94098b1e9e70face7a4b8d974fddf
hyu <1415515984yuri@gmail.com>
2022-03-02 18:01:26 +0800

    chore: 添加v-visible 指令，控制元素显隐
    - 修改css visibility 属性

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,1 +1,13 @@
-export { default as permission } from './permisson';
+// export { default as permission } from './permisson';
+
+export const visible = {
+  bind: () => {},
+  inserted: (el, binding) => {
+    const { value = true } = binding;
+    el.style.visibility = value ? 'visible' : 'hidden';
+  },
+  update: (el, binding) => {
+    const { value = true } = binding;
+    el.style.visibility = value ? 'visible' : 'hidden';
+  },
+};
