83acb58afed01399aef45f478b113d0451b9055e
gumengyu <1415515984yuri@gmail.com>
2022-05-13 17:46:34 +0800

    port to v3

---
+++ packages/renderer/src/views/Daily.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="title mb-5">
+      <div class="text-caption grey--text font-weight-bold">
+        {{ date }}
+      </div>
+      <div class="d-flex justify-space-between">
+        <div class="text-h6 font-weight-bold">
+          {{ $t('main.nav.daily') }}
+        </div>
+        <v-btn size="small" :loading="loading" color="primary" @click="play">
+          <v-icon> {{ icon.mdiPlay }}</v-icon>
+        </v-btn>
+      </div>
+    </div>
+    <v-list>
+      <TrackItem v-for="track in daily" :key="track.id" :track="track"> </TrackItem>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { mdiPlay } from '@mdi/js'
+import dayjs from 'dayjs'
+import { defineComponent, ref } from 'vue'
+
+import { getDailyRecommend } from '@/api/user'
+import TrackItem from '@/components/app/TrackItem.vue'
+import { usePlayer } from '@/player/player'
+import type { TrackSource } from '@/types'
+export default defineComponent({
+  name: 'Daily',
+  components: { TrackItem },
+  setup() {
+    const player = usePlayer()
+    const loading = ref(false)
+    const daily = ref<TrackSource[]>([])
+    async function play() {
+      loading.value = true
+      await player.updateTracks({ list: daily.value }, true)
+      loading.value = false
+    }
+    return {
+      daily,
+      play,
+      loading,
+    }
+  },
+  data: () => ({
+    icon: {
+      mdiPlay,
+    },
+    date: dayjs().format('MM/DD'),
+  }),
+  created() {
+    this.fetch()
+  },
+  methods: {
+    async fetch() {
+      const { data } = await getDailyRecommend()
+      this.daily = data?.dailySongs ?? []
+    },
+  },
+})
+</script>
