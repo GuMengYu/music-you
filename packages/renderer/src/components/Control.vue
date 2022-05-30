30c6a65a98836ec87fc87814fe5b29679e79c373
yuhong <1415515984yuri@gmail.com>
2022-05-30 20:26:56 +0800

    chore: add track loading loader

--- packages/renderer/src/components/Control.vue
+++ packages/renderer/src/components/Control.vue
@@ -1,47 +1,3 @@
-<template>
-  <div class="d-flex justify-center align-center control-buttons gap-2">
-    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="toggleShuffle">
-      <v-icon size="x-small">
-        {{ shuffle ? icon.mdiShuffle : icon.mdiShuffleDisabled }}
-      </v-icon>
-    </v-btn>
-    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="prev">
-      <v-icon size="x-small">{{ icon.mdiSkipPrevious }}</v-icon>
-    </v-btn>
-
-    <v-btn
-      icon
-      class="play-fab"
-      :class="{ playing }"
-      size="small"
-      color="primary"
-      :loading="loadingTrack"
-      variant="contained-flat"
-      @click="togglePlay"
-    >
-      <lottie-icon
-        class="lottie-icon"
-        style="position: relative; top: 1px"
-        :options="playOptions"
-        :width="30"
-        :height="30"
-        @anim-created="handleAnimation"
-      ></lottie-icon>
-      <!--      <v-icon :size="24" color="onPrimary">-->
-      <!--        {{ playingState.icon }}-->
-      <!--      </v-icon>-->
-    </v-btn>
-
-    <v-btn icon flat size="small" @click="next">
-      <v-icon size="x-small">{{ icon.mdiSkipNext }}</v-icon>
-    </v-btn>
-    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="switchMode">
-      <v-icon size="x-small">
-        {{ orderIconState }}
-      </v-icon>
-    </v-btn>
-  </div>
-</template>
 <script setup lang="ts">
 import {
   mdiRepeat,
@@ -57,6 +13,7 @@ import { computed } from 'vue'
 
 import { usePlayer } from '@/player/player'
 import { PLAY_MODE, usePlayerStore } from '@/store/player'
+import { playToPause as playToPauseAnimationData } from '@/util/animationData.json'
 
 const playerStore = usePlayerStore()
 const player = usePlayer()
@@ -73,6 +30,13 @@ const orderIconState = computed(() => {
     }[playMode.value as string] ?? mdiRepeat
   )
 })
+const playAnim = ref()
+const playOptions = {
+  animationData: playToPauseAnimationData,
+  loop: false,
+  autoplay: false,
+}
+
 const switchMode = () => {
   const mode = playMode.value as string
   if (mode === PLAY_MODE.DISABLE) {
@@ -88,63 +52,70 @@ const togglePlay = () => {
   playing.value = !playing.value
 }
 
-const next = () => {
+watch(playing, (val) => {
+  if (val) {
+    playAnim.value?.playSegments([0, 30], true)
+  } else {
+    playAnim.value?.playSegments([30, 60], true)
+  }
+})
+function next() {
   player.next()
 }
-const prev = () => {
+function prev() {
   player.prev()
 }
-</script>
-<script lang="ts">
-import { playToPause as playToPauseAnimationData } from '@/util/animationData.json'
 
-export default {
-  name: 'Control',
-  data() {
-    return {
-      icon: {
-        mdiSkipNext,
-        mdiSkipPrevious,
-        mdiShuffle,
-        mdiShuffleDisabled,
-      },
-      playAnim: null,
-      playOptions: {
-        animationData: playToPauseAnimationData,
-        loop: false,
-        autoplay: false,
-      },
-    }
-  },
-  computed: {},
-  watch: {
-    // update play icon state
-    async playing(val) {
-      await this.$nextTick()
-      if (val) {
-        this.playAnim?.playSegments([0, 30], true)
-      } else {
-        this.playAnim?.playSegments([30, 60], true)
-      }
-    },
-  },
-  methods: {
-    handleAnimation(animation) {
-      this.playAnim = animation
-      this.playAnim.setSpeed(2)
-    },
-    toggleShuffle() {
-      this.shuffle = !this.shuffle
-    },
-  },
+function handleAnimation(animation) {
+  playAnim.value = animation
+  playAnim.value.setSpeed(2)
+}
+function toggleShuffle() {
+  // todo shuffle
 }
 </script>
-<style lang="scss" scoped>
-.control-buttons {
-  .play-fab {
-    &.playing {
-      animation: pulseWarn 1.5s infinite;
-    }
-  }
-}
-</style>
+<template>
+  <div class="d-flex justify-center align-center control-buttons gap-2">
+    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="toggleShuffle">
+      <v-icon size="x-small">
+        {{ shuffle ? mdiShuffle : mdiShuffleDisabled }}
+      </v-icon>
+    </v-btn>
+    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="prev">
+      <v-icon size="x-small">{{ mdiSkipPrevious }}</v-icon>
+    </v-btn>
+
+    <v-btn
+      icon
+      class="play-fab"
+      :style="{
+        animation: playing ? 'pulseWarn 1.5s infinite' : '',
+      }"
+      size="small"
+      color="primary"
+      :loading="loadingTrack"
+      variant="contained-flat"
+      @click="togglePlay"
+    >
+      <v-progress-circular v-if="loadingTrack" indeterminate color="onPrimary" />
+      <lottie-icon
+        v-else
+        class="lottie-icon"
+        style="position: relative; top: 1px"
+        :options="playOptions"
+        :width="30"
+        :height="30"
+        @anim-created="handleAnimation"
+      ></lottie-icon>
+    </v-btn>
+
+    <v-btn icon flat size="small" @click="next">
+      <v-icon size="x-small">{{ mdiSkipNext }}</v-icon>
+    </v-btn>
+    <v-btn icon :disabled="isCurrentFm" flat size="small" @click="switchMode">
+      <v-icon size="x-small">
+        {{ orderIconState }}
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
