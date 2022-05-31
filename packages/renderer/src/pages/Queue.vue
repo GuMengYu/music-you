47362f3d098994c2c5a2d36c6a442352dc8207da
gumengyu <1415515984yuri@gmail.com>
2022-05-31 14:56:40 +0800

    perf: queue

--- packages/renderer/src/pages/Queue.vue
+++ packages/renderer/src/pages/Queue.vue
@@ -1,11 +1,12 @@
 <template>
   <section>
+    {{ index }}
     <h1 class="text-h6">{{ $t('common.queue') }}</h1>
     <h2 class="text-subtitle-2 my-2">{{ $t('common.current_play') }}</h2>
     <track-item v-if="current" :track="current" :index="1" type="list" />
     <h2 class="text-subtitle-2 my-2">{{ $t('common.next_from') }}</h2>
-    <track-list :tracks="playingList.list" type="list"> </track-list>
-    <template v-if="!playingList.list.length">
+    <track-list :tracks="nextList" type="list"> </track-list>
+    <template v-if="!nextList.length">
       <p class="d-flex justify-center font-weight-bold">
         {{ $t('common.empty_playing_list') }}
       </p>
@@ -20,4 +21,14 @@ import { usePlayerStore } from '@/store/player'
 
 const playerStore = usePlayerStore()
 const { playingList, track: current } = storeToRefs(playerStore)
+
+const index = computed(() => {
+  const index = playingList.value.list.findIndex((track) => track.id === current.value?.id)
+  return index + 1
+})
+const nextList = computed(() => {
+  const list = playingList.value.list
+  if (!list || !list.length) return []
+  return list.slice(index.value)
+})
 </script>
