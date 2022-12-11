cdba7e4475ba170021734ac596cf37983031de3c
是一刀斩哒 <yidaozhanyaqwq@gmail.com>
2022-12-11 18:20:01 +0800

    fix: 在 Linux kernel 6.1 上被误认为是 Windows 7 的问题

--- packages/main/index.ts
+++ packages/main/index.ts
@@ -102,7 +102,7 @@ function preCheck() {
   protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }])
   process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
   // Disable GPU Acceleration for Windows 7
-  if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+  if (is.windows() && release().startsWith('6.1')) app.disableHardwareAcceleration()
 
   // Set application name for Windows 10+ notifications
   if (process.platform === 'win32' || process.platform === 'linux') app.setAppUserModelId(app.getName())
