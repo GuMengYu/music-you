06463708aeac608b9be011595ec3db1e2de48752
gumengyu <1415515984yuri@gmail.com>
2023-01-30 18:25:44 +0800

    fix: close app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,17 +6,16 @@
 mod cmds;
 mod utils;
 
-use tauri::{
-    api::process::{Command},
-  };
+use tauri::{api, api::process::{Command}, Manager, Wry};
 
 use tauri_utils::config::{Config, WindowConfig};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 
-fn main() {
-    tauri::Builder::default()
+fn main() -> std::io::Result<()> {
+    println!("app start");
+    let mut builder = tauri::Builder::default()
         .setup(|_app| {
             tauri::async_runtime::spawn(async move {
                 Command::new_sidecar("app")
@@ -30,7 +29,36 @@ fn main() {
             cmds::close_splashscreen,
             cmds::minimized,
             cmds::open_web_url,
-        ])
-        .run(tauri::generate_context!())
+        ]);
+
+    let app = builder
+        .build(tauri::generate_context!())
         .expect("error while running tauri application");
+    app.run(|app_handle, e| match e {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        tauri::RunEvent::Exit => {
+            api::process::kill_children();
+            app_handle.exit(0);
+        }
+        #[cfg(target_os = "macos")]
+        tauri::RunEvent::WindowEvent { label, event, .. } => {
+            use tauri::Manager;
+
+            if label == "main" {
+                match event {
+                    tauri::WindowEvent::CloseRequested { api, .. } => {
+                        api.prevent_close();
+                        app_handle.get_window("main").map(|win| {
+                            let _ = win.hide();
+                        });
+                    }
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
+    });
+    Ok(())
 }
