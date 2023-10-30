8fc6b954c8b3dfbb4ca14fe9a68a300dd3a6e319
gumengyu <1415515984yuri@gmail.com>
2023-10-30 08:48:17 +0800

    fix: react devtools

--- electron/main/core/util/extensions.ts
+++ electron/main/core/util/extensions.ts
@@ -4,12 +4,10 @@ import is from 'electron-is'
 import log from './log'
 
 export async function installExtensions() {
-  if (is.dev() && !process.env.IS_TEST) {
-    // Install Vue Devtools
+  if (is.dev()) {
+    // Install React Devtools
     try {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      await installExtension(REACT_DEVELOPER_TOOLS)
+      await installExtension(REACT_DEVELOPER_TOOLS.id)
     }
     catch (e) {
       log.error('Vue Devtools failed to install:', e)
